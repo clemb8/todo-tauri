a73661dcc22b838a3fe54a38d9ff3912d2975ac8
clemb8 <clem.boschet@gmail.com>
2022-09-21 23:37:18 +0200

    List Component

--- src/List.tsx
+++ src/List.tsx
@@ -2,14 +2,16 @@ import { Todo } from "./models/Todo";
 import "./List.css";
 
 interface PropsList {
-  todos: Todo[]
+  todos: Todo[],
+  onAdd: () => void
 }
 
-function List({ todos }: PropsList) {
+function List({ todos, onAdd }: PropsList) {
 
   return(
     <div className="todoList">
       <h1>My To-Do List</h1>
+      <button className="button-add" onClick={onAdd}>Add a To-Do</button>
       <div className="items">
 
         <h2 className="done">Done</h2>
@@ -17,7 +19,7 @@ function List({ todos }: PropsList) {
           todos.map((todo) => {
             if(todo.done) {
               return(
-                <><input id={todo.id} type="checkbox" checked /><label className="done" htmlFor={todo.id}>{ todo.title }</label></>
+                <><input className="checkbox" id={todo.id} type="checkbox" checked /><label className="done" htmlFor={todo.id}>{ todo.title }</label></>
               )
             }
           }) 
@@ -28,7 +30,7 @@ function List({ todos }: PropsList) {
           todos.map((todo) => {
             if(!todo.done) {
               return(
-                <><input id={todo.id} type="checkbox" /><label className="pending" htmlFor={todo.id}>{ todo.title }</label></>
+                <><input className="checkbox" id={todo.id} type="checkbox" /><label className="pending" htmlFor={todo.id}>{ todo.title }</label></>
               )
             }
           }) 
